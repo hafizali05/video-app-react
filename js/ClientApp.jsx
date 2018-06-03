7c984afddb06e293c6c28bca40b19accedfa5f37
Hafiz <hafizdidarali@gmail.com>
2018-06-03 10:04:50 +0100

    created react component with button

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,29 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
-const MyTitle = function (props) {
-  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-
-  const style = { color: props.color };
+const App = () => {
   return (
-    <div>
-      <h1 style={style}>{props.title}</h1>
+    <div className="app">
+      <div className="landing">
+        <h1>s video</h1>
+        <input type="text" placeholder="search videos" />
+        <a>hello world</a>
+      </div>
     </div>
   )
-};
+}
 
-const MyFirstComponent = function () {
-  return (
-    <div>
-      <MyTitle title='Game of Thrones hafiz' color='YellowGreen' />
-      <MyTitle title='Stranger Things alid' color='GreenYellow' />
-      <MyTitle title='Rick and Morty dad' color='LimeGreen' />
-      <MyTitle title='House of Cards mom' color='peru' />
-      <MyTitle title='blue lions' color='blue' />
-    </div>
-  )
-};
-
-render(<MyFirstComponent />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
