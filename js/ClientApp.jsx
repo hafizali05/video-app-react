804647412032469dd1c82e23abb141991a5a47c9
Hafiz <hafizdidarali@gmail.com>
2018-06-03 10:10:34 +0100

    implicit returning App

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="landing">
-        <h1>s video</h1>
-        <input type="text" placeholder="search videos" />
-        <a>hello world</a>
-      </div>
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>s video</h1>
+      <input type="text" placeholder="search videos" />
+      <a>hello world</a>
     </div>
-  )
-}
+  </div>
+)
 
 render(<App />, document.getElementById('app'));
