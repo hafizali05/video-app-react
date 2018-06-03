3ff76359fec2b1f789f0cd6549ef90537c96d931
Hafiz <hafizdidarali@gmail.com>
2018-06-03 09:59:56 +0100

    working commits

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,19 +3,27 @@ import { render } from 'react-dom';
 
 const ce = React.createElement;
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+const MyTitle = function (props) {
+  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+
+  const style = { color: props.color };
+  return (
+    <div>
+      <h1 style={style}>{props.title}</h1>
+    </div>
+  )
 };
 
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'House of Cards', color: 'peru' })
-  );
+const MyFirstComponent = function () {
+  return (
+    <div>
+      <MyTitle title='Game of Thrones hafiz' color='YellowGreen' />
+      <MyTitle title='Stranger Things alid' color='GreenYellow' />
+      <MyTitle title='Rick and Morty dad' color='LimeGreen' />
+      <MyTitle title='House of Cards mom' color='peru' />
+      <MyTitle title='blue lions' color='blue' />
+    </div>
+  )
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent />, document.getElementById('app'));
