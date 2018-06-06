2e0c303b4952b87ae0acba8f33e1a817a0b9c1bc
Hafiz <hafizdidarali@gmail.com>
2018-06-06 14:54:18 +0100

    styled component

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,8 +3,10 @@ import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
 const Search = () => (
-  <div>
-    {preload.shows.map(show => <ShowCard {...show} key={show.imdbID}/>)}
+  <div className="search">
+    <div>
+      {preload.shows.map(show => <ShowCard {...show} key={show.imdbID} />)}
+    </div>
   </div>
 );
 
--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,26 +1,41 @@
 import React from "react";
-import { shape, string } from "prop-types";
+import { string } from "prop-types";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;  
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
 
 const ShowCard = props => (
-  <div className="show-card">
-    <img
-      src={`../public/img/posters/${props.poster}`}
+  <Wrapper>
+    <Image
       alt={`${props.title} show poster`}
+      src={`../public/img/posters/${props.poster}`}
     />
-    <h3>{props.title}</h3>
-    <h4>{props.year}</h4>
-    <p>{props.description}</p>
-  </div>
+    <div className="show-card">
+      <h3>{props.title}</h3>
+      <h4>{props.year}</h4>
+      <p>{props.description}</p>
+    </div>
+  </Wrapper>
 );
 
-// ShowCard.propTypes = {
-//   show: shape({
-//     poster:string.isRequired,
-//     title: string.isRequired,
-//     year: string.isRequired,
-//     description: string.isRequired,
-//   }).isRequired
-// };
-
+ShowCard.propTypes = {
+  poster: string.isRequired,
+  title: string.isRequired,
+  year: string.isRequired,
+  description: string.isRequired
+};
 
 export default ShowCard;
